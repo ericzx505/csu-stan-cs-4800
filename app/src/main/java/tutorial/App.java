876f3e3a826670432ef053159ac2de6c13cbe8ce
Cveloz2020 <162510000+Cveloz2020@users.noreply.github.com>
2024-03-06 13:57:08 -0800

    Hello added

--- app/src/main/java/tutorial/App.java
+++ app/src/main/java/tutorial/App.java
@@ -5,7 +5,7 @@ package tutorial;
 
 public class App {
     public String getGreeting() {
-        return "Goodbye Space";
+        return "Hello Goodbye Space";
     }
 
     public static void main(String[] args) {
